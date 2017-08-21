4a10fab4b550c5f9d47d0f7ece868324fe3c2ed5
dpietrzak1990 <dpietrzak1990@o2.pl>
2017-08-21 21:01:51 +0200

    Kompozycja i agregacja

---
+++ src/MyPointAndMyCircle/Main.java
@@ -0,0 +1,12 @@
+package MyPointAndMyCircle;
+
+/**
+ * Created by RENT on 2017-08-21.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyPoint mpoint1 = new MyPoint(1, 1);
+        MyPoint mpoint2 = new MyPoint(2, 2);
+        mpoint1.distance(mpoint2);
+    }
+}
